083145c211494646177e2e909c44fd4d3b95e3d8
erinz2020 <erinz2020@gmail.com>
2024-02-10 01:51:16 +0000

    fake api

---
+++ copyReact.sh
@@ -0,0 +1,2 @@
+cd /home/ubuntu/workspace/Wildbook/src/main/webapp/frontend/ && npm run build
+sudo cp -r /home/ubuntu/workspace/Wildbook/src/main/webapp/frontend/build/* /var/lib/tomcat9/webapps/wildbook/react/
