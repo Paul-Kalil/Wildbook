54c9b382a39f140670771dc2a0b237c4831f7438
erinz2020 <erinz2020@gmail.com>
2024-02-13 00:45:44 +0000

    cra template

---
+++ src/main/webapp/frontendBak/src/About.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+        <h1>About</h1>
+        <p>
+            This is a simple React app that demonstrates how to use Spring Boot and React.
+        </p>
+        </div>
+    );
+}
